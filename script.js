3cd186786298b9b1914d1ace6c542e16bcd85e9e
Andrzej Hnatiuk <68433600+andrzejhnatiuk@users.noreply.github.com>
2022-01-22 16:34:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const h1 = document.querySelector("h1")
+const button = document.querySelector("button")
+
+button.addEventListener("click", ()=>{
+    const r = Math.floor(Math.random()*255)
+    const g = Math.floor(Math.random()*255)
+    const b = Math.floor(Math.random()*255)
+    const newColor = `rgb(${r},${g},${b})`
+    document.body.style.backgroundColor = newColor
+    h1.innerText = newColor
+});
